0f0c6b561ef2c14d8e729ceab57e537d051abfb5
KennyCruzer <kennycruz@Kenneths-MBP.fios-router.home>
2020-07-01 16:58:48 -0400

    updates on ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,18 @@ import SEO from "../components/seo"
 
 const IndexPage = () => {
   return(
-  <Layout>
-  <SEO title="Home" />
-  <div className="landing">
-    <h3>Visual Artist</h3>
-    <h3>Typography</h3>
-    <h3>Print Work</h3>
-    <h3>Branding</h3>
-    <h3>Software Development</h3>
-    <h3>UI/UX</h3>
-    <h3>Packaging Design</h3>
-  </div>
-  </Layout>
+    <Layout>
+    <SEO title="Home" />
+    <div className="landing">
+      <h3>Visual Artist</h3>
+      <h3>Typography</h3>
+      <h3>Print Work</h3>
+      <h3>Branding</h3>
+      <h3>Software Development</h3>
+      <h3>UI/UX</h3>
+      <h3>Packaging Design</h3>
+    </div>
+    </Layout>
   )
 }
 
